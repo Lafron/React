77d8d356d0e0388b76deebbabecc1068e4c60e98
Epshtein Dmitry <letuver@yahoo.com>
2022-04-09 01:53:45 +0300

    fix error

--- src/components/quality.jsx
+++ src/components/quality.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-const setQualities = quality => {
+const setQualities = (quality) => {
     let classes = "badge m-1 bg-";
     classes += quality.color;
-        
+
     return (
-        <span key = {quality._id} className={classes}>
-                {quality.name}
+        <span key={quality._id} className={classes}>
+            {quality.name}
         </span>
-      )
-    };
+    );
+};
 
 export default setQualities;
--- src/components/searchStatus.jsx
+++ src/components/searchStatus.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 
-const RenderPhrase = number => {
-    if(number > 0){
-            return(
-                <span className="text-light bg-primary p-2">
-                    {number} человек сегодня тусанут с табой
-                </span>
-            )
-        }
-        else{
-            return(
-                <span className="text-light bg-danger p-2">
-                     Никто с табой не тусанёт
-                </span>
-            );
-    }        
+const RenderPhrase = (number) => {
+    if (number > 0) {
+        return (
+            <span className="text-light bg-primary p-2">
+                {number} человек сегодня тусанут с табой
+            </span>
+        );
+    } else {
+        return (
+            <span className="text-light bg-danger p-2">
+                Никто с табой не тусанёт
+            </span>
+        );
+    }
 };
 
 export default RenderPhrase;
